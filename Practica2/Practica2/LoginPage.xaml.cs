2a566a47db5bc22d81108d0196816d4525c5663b
AndreinaJc97 <andreinajc97@gmail.com>
2020-11-19 11:50:59 -0400

    new app

---
+++ Practica2/Practica2/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            var email = EntryEmail.Text;
+
+            if (true)
+            {
+
+            }
+        }
+
+        private async void RegisterPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+    }
+}
